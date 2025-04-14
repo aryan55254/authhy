31f0c635e0a46230cf05796e6b0db0c86930cdd7
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-04-14 22:26:28 +0530

    starting again

--- backend/utils/generateOTP.js
+++
@@ -1,8 +0,0 @@
-//otp generation
-
-const generateOTP = () => {
-  const otp = Math.floor(100000 + Math.random() * 900000).toString();
-  return otp;
-};
-
-module.exports = generateOTP;
--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
-const generatetoken = (user) => {
-  return jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
-  });
-};
-
-module.exports = generatetoken;
